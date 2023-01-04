16e116ec3a80723cfd287b76a5d6816fbf23bff5
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-04 22:41:59 +0400

    jenkins

---
+++ vars/java.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+    stages {
+
+        stage('compile'){
+            steps {
+                echo 'compile'
+            }
+        }
+        stage('code quality'){
+            steps {
+                echo 'code quality'
+            }
+        }
+
+        stage('Test cases'){
+            steps {
+                echo 'Test cases'
+            }
+        }
+
+        stage('Publish release'){
+            steps {
+                echo 'Publish release'
+            }
+        }
+    }
+}
---
+++ vars/python.groovy
@@ -0,0 +1,26 @@
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('code quality'){
+                steps {
+                    echo 'code quality'
+                }
+            }
+
+            stage('Test cases'){
+                steps {
+                    echo 'Test cases'
+                }
+            }
+
+            stage('Publish release'){
+                steps {
+                    echo 'Publish release'
+                }
+            }
+        }
+    }
+}
