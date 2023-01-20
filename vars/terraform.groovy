110fb7e609d742a952ae95adc04dc33d3e6d000e
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-21 01:28:35 +0400

    tera

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,5 +1,34 @@
 def call() {
-    node {
-        common.checkout()
-    }
+   pipeline {
+       agent any
+
+       options {
+           ansiColor('xterm')
+       }
+       parameters {
+           choice(name: 'ENVIRONMENT', choices: ['', 'dev', 'prod'], description: 'Pick ENVIRONMENT')
+           choice(name: 'ACTION', choices: ['', 'apply', 'destroy'], description: 'Pick TERRAFORM ACTION')
+       }
+       stages {
+          stage('terraform init') {
+               steps {
+                   sh '''
+                   terraform init -backend-config=env/${COMPONENT}-backend.tfvars
+                   '''
+                 }
+             }
+         }
+       stages {
+           stage('terraform plan') {
+               steps {
+                   sh '''
+                   terraform plan -var-file=env/${COMPONENT}.tfvars
+                   '''
+               }
+           }
+       }
+
+   }
+
+  }
 }
