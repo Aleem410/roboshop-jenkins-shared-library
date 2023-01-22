e954f72ddf78f3236a4d0f01797f3c2b2cf8576d
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-22 16:04:28 +0400

    robo

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,36 +1,36 @@
 def call() {
    node {
-     properties([
-       parameters([
+       properties([
+         parameters([
            [$class: 'ChoiceParameterDefinition',
            choices: '\ndev\nprod\n',
            name: 'ENVIRONMENT',
            description: "choose Environment"
            ],
-       ]),
-       parameters([
+         ]),
+         parameters([
            [$class: 'ChoiceParameterDefinition',
            choices: '\napply\ndestroy\n',
            name: 'ACTION',
            description: "choose Action"
            ],
-       ]),
-     ])
+         ]),
+       ])
        stage('terraform init') {
            sh '''
               terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
            '''
-             }
-          stage('terraform plan') {
+       }
+       stage('terraform plan') {
            sh '''
               terraform plan -var-file=env/${ENVIRONMENT}.tfvars
            '''
-           }
-           stage('terraform Apply') {
+       }
+       stage('terraform Apply') {
            sh '''
               terraform apply -auto-approve -var-file=env/${ENVIRONMENT}.tfvars
            '''
-           }
+       }
 
    }
 }
