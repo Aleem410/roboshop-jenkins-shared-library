def651c2d199c2155d3a2588bbdcb75c3e128462
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-22 15:58:46 +0400

    robo

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,21 +1,21 @@
 def call() {
    node {
-       properties([
-               parameters([
-                       [$class: 'ChoiceParameterDefinition',
-                        choices: '\ndev\nprod\n',
-                        name: 'ENVIRONMENT',
-                        description: "choose Environment"
-                       ],
-               ]),
-               parameters([
-                       [$class: 'ChoiceParameterDefinition',
-                        choices: '\napply\ndestroy\n',
-                        name: 'ACTION',
-                        description: "choose Action"
-                       ],
-               ]),
-       ])
+     properties([
+       parameters([
+           [$class: 'ChoiceParameterDefinition',
+           choices: '\ndev\nprod\n',
+           name: 'ENVIRONMENT',
+           description: "choose Environment"
+           ],
+       ]),
+       parameters([
+           [$class: 'ChoiceParameterDefinition',
+           choices: '\napply\ndestroy\n',
+           name: 'ACTION',
+           description: "choose Action"
+           ],
+       ]),
+     ])
        stage('terraform init') {
            sh '''
               terraform init -backend-config=env/${ENVIRONMENT}-backend.tfvars
